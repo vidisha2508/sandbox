0928ecb99b94c0c4df9c9cbf6610de6c18d8cf8b
Vidisha Jain <vj251099@gmail.com>
2026-07-05 00:09:46 +0530

    v0.7 managing cam with grid
    Implement CameraManager class for video handling

---
+++ cameraManager.js
@@ -0,0 +1,41 @@
+export class CameraManager {
+
+    constructor() {
+
+        this.video = document.createElement("video");
+        this.video.autoplay = true;
+        this.video.playsInline = true;
+        this.video.muted = true;
+
+    }
+
+    async start() {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+
+            video: true,
+            audio: false
+
+        });
+
+        this.video.srcObject = stream;
+        await this.video.play();
+
+        return this.video;
+
+    }
+
+    stop() {
+
+        const stream = this.video.srcObject;
+
+        if (stream) {
+
+            stream.getTracks().forEach(track => track.stop());
+
+        }
+
+        this.video.srcObject = null;
+
+    }
+}
