a41777115b9830c449d972237b66cebcd4c4954d
Vidisha Jain <vj251099@gmail.com>
2026-07-05 00:16:48 +0530

    v0.7WebGLRenderer
    Added alpha support and adjusted renderer DOM element styles.

--- renderer.js
+++ renderer.js
@@ -1,18 +1,28 @@
 import * as THREE from "three";
 
 export const renderer = new THREE.WebGLRenderer({
-    antialias: true
-});
 
-renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-);
+    antialias: true,
+    alpha: true
+
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 
+renderer.setSize(
+
+    window.innerWidth,
+    window.innerHeight
+
+);
+
+renderer.setClearColor(0x000000, 0);
+
 renderer.shadowMap.enabled = true;
 
-document
-    .getElementById("scene")
-    .appendChild(renderer.domElement);
+renderer.domElement.style.position = "absolute";
+renderer.domElement.style.top = "0";
+renderer.domElement.style.left = "0";
+renderer.domElement.style.width = "100%";
+renderer.domElement.style.height = "100%";
+renderer.domElement.style.zIndex = "1";
